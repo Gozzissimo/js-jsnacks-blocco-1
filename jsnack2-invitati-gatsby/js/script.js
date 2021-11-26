9712a80e139b2bab6954801b84bf61f14e8b1892
Gozzissimo <fantini.nico@gmail.com>
2021-11-26 16:23:25 +0100

    final con Includes e ciclo For

---
+++ jsnack2-invitati-gatsby/js/script.js
@@ -0,0 +1,38 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+invitedList = [
+    'Marco',
+    'Chiara',
+    'Francesco',
+    'Topolino',
+    'Ciro',
+    'Lele',
+    'Ciccio Graziani'
+]
+
+const userName = prompt('Inserisci il tuo nome');
+
+// METODO 1 OK - INCLUDES + IF
+// const invited = invitedList.includes(userName);
+
+// if (invited) {
+//     alert('Prego può entrare');
+// } else {
+//     alert('Non può entare');
+// }
+
+
+// METODO 2 OK - BOOLEAN + FOR + IF
+let find = false 
+
+for (let i = 0; i < invitedList.length; i++) {
+    if (userName == invitedList[i]) {
+        find = true;
+    }
+}
+
+if (find == true) {
+    alert('Prego può entrare');
+} else {
+    alert('Non può entare');
+}
